43c22619b8f2ad1e610fc3498a3e61414d8f2a7b
Eugene Burkov <e.burkov@adguard.com>
2026-09-15 19:23:02 +0300

    github: use better checkout

--- scripts/make/version.sh
+++ scripts/make/version.sh
@@ -24,13 +24,10 @@ set -e -f -u
 channel="${CHANNEL:?please set CHANNEL}"
 readonly channel
 
-head="${HEAD:-$(git rev-parse --short HEAD)}"
-readonly head
-
 case "$channel" in
 'development')
 	# commit_number is the number of current commit within the branch.
-	commit_number="$(git rev-list --count master.."${head}" --)"
+	commit_number="$(git rev-list --count master..HEAD --)"
 	readonly commit_number
 
 	# The development builds are described with a combination of unset semantic
@@ -38,7 +35,7 @@ case "$channel" in
 	#
 	#   v0.0.0-dev.5-a1b2c3d4
 	#
-	version="v0.0.0-dev.${commit_number}+${head}"
+	version="v0.0.0-dev.${commit_number}+$(git rev-parse --short HEAD)"
 	;;
 'release')
 	# current_desc is the description of the current git commit.  If the
@@ -64,7 +61,7 @@ case "$channel" in
 	# candidate builds.
 
 	# current_branch is the name of the branch currently checked out.
-	current_branch="$(git rev-parse --abbrev-ref "${head}")"
+	current_branch="$(git rev-parse --abbrev-ref HEAD)"
 	readonly current_branch
 
 	# The branch should be named like:
@@ -77,10 +74,7 @@ case "$channel" in
 		exit 1
 	fi
 
-	commit_number="$(git rev-list --count master.."${head}" --)"
-	readonly commit_number
-
-	version="${current_branch#rc-}-rc.${commit_number}"
+	version="${current_branch#rc-}-rc.$(git rev-list --count "master"..HEAD)"
 	;;
 *)
 	echo "invalid channel '$channel', supported values are \
